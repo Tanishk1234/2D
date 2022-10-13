bf55d6b6b7872b787ec552b51faadb10c3d7fc83
Tanishk1234 <72665545+Tanishk1234@users.noreply.github.com>
2022-10-13 16:25:13 +0530

    Update BackGroundScroll.cs

--- 2d sripts/BackGroundScroll.cs
+++ 2d sripts/BackGroundScroll.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+// created by a tanishk agrawal which lives in mathura to develop its game so ask them first to use this code
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
